255ef53fb0bbea190068e6a70e7572de1c517a02
Kamchai Boonruam <kamchai2013@gmail.com>
2018-01-16 03:26:10 +0000

    Fix

--- clear_disk.sh
+++ clear_disk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
  
-set_max=80
+set_max=4
 work_dir="/var/log"
 
 used_space=`df -H $work_dir  | grep % | awk {'print $5'} | sed '1d;s/[^0-9]//g'`
@@ -9,8 +9,11 @@ used_space=`df -H $work_dir  | grep % | awk {'print $5'} | sed '1d;s/[^0-9]//g'`
 if [ $used_space -gt $set_max ]
 then
     echo "Clear Disk"
-    truncate -s 0 $work_dir/access.*
-    
+    #truncate -s 0 $work_dir/access.*
+    find $work_dir  -type f -name 'access.*' -print | while read i 
+    do 
+        cat /dev/null > "$i"
+    done
 else
    echo "Disk OK"
 fi
